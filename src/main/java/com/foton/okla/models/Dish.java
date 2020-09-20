64167775842374cdb9ce4f321edbf17eb205c1d8
maher.ben@foton-it.com <maher.ben@foton-it.com>
2020-09-20 20:29:07 +0100

    Add Dish validation

--- src/main/java/com/foton/okla/models/Dish.java
+++ src/main/java/com/foton/okla/models/Dish.java
@@ -9,6 +9,12 @@ import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
 import javax.persistence.Table;
 
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
+
+import lombok.Data;
+
+@Data
 @Entity
 @Table(name = "dishes")
 public class Dish {
@@ -18,15 +24,23 @@ public class Dish {
 
 	private String dbUri;
 
+	@Column(name = "fromdb")
 	private boolean fromDb;
-
+	
+	
+	@NotNull
+	@NotBlank
 	private String label;
 
+	@NotNull
+	@NotBlank
 	@Column(columnDefinition = "text")
 	private String description;
 
+	@NotNull
 	@ElementCollection
 	private List<String> types;
+	
 	private String inventor;
 	private String calories;
 
@@ -41,6 +55,7 @@ public class Dish {
 
 	private String preparationVideo;
 
+	@NotNull
 	@ElementCollection
 	private List<String> ingredients;
 
@@ -58,149 +73,4 @@ public class Dish {
 
 	@ElementCollection
 	private List<String> chefs;
-
-	public Long getId() {
-		return id;
-	}
-
-	public void setId(Long id) {
-		this.id = id;
-	}
-
-	public String getDbUri() {
-		return dbUri;
-	}
-
-	public void setDbUri(String dbUri) {
-		this.dbUri = dbUri;
-	}
-
-	public boolean isFromDb() {
-		return fromDb;
-	}
-
-	public void setFromDb(boolean fromDb) {
-		this.fromDb = fromDb;
-	}
-
-	public String getLabel() {
-		return label;
-	}
-
-	public void setLabel(String label) {
-		this.label = label;
-	}
-
-	public String getDescription() {
-		return description;
-	}
-
-	public void setDescription(String description) {
-		this.description = description;
-	}
-
-	public List<String> getTypes() {
-		return types;
-	}
-
-	public void setTypes(List<String> types) {
-		this.types = types;
-	}
-
-	public String getInventor() {
-		return inventor;
-	}
-
-	public void setInventor(String inventor) {
-		this.inventor = inventor;
-	}
-
-	public String getCalories() {
-		return calories;
-	}
-
-	public void setCalories(String calories) {
-		this.calories = calories;
-	}
-
-	public List<String> getSubjects() {
-		return subjects;
-	}
-
-	public void setSubjects(List<String> subjects) {
-		this.subjects = subjects;
-	}
-
-	public List<String> getImages() {
-		return images;
-	}
-
-	public void setImages(List<String> images) {
-		this.images = images;
-	}
-
-	public String getPreparationSheet() {
-		return preparationSheet;
-	}
-
-	public void setPreparationSheet(String preparationSheet) {
-		this.preparationSheet = preparationSheet;
-	}
-
-	public String getPreparationVideo() {
-		return preparationVideo;
-	}
-
-	public void setPreparationVideo(String preparationVideo) {
-		this.preparationVideo = preparationVideo;
-	}
-
-	public List<String> getIngredients() {
-		return ingredients;
-	}
-
-	public void setIngredients(List<String> ingredients) {
-		this.ingredients = ingredients;
-	}
-
-	public List<String> getCountries() {
-		return countries;
-	}
-
-	public void setCountries(List<String> countries) {
-		this.countries = countries;
-	}
-
-	public List<String> getVariants() {
-		return variants;
-	}
-
-	public void setVariants(List<String> variants) {
-		this.variants = variants;
-	}
-
-	public List<Dish> getSimilarDishes() {
-		return similarDishes;
-	}
-
-	public void setSimilarDishes(List<Dish> similarPlats) {
-		this.similarDishes = similarPlats;
-	}
-
-	public List<String> getCompanies() {
-		return companies;
-	}
-
-	public void setCompanies(List<String> companies) {
-		this.companies = companies;
-	}
-
-	public List<String> getChefs() {
-		return chefs;
-	}
-
-	public void setChefs(List<String> chefs) {
-		this.chefs = chefs;
-	}
-
 }
